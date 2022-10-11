ed084bcbb56b6920e008e1a595373875204fa726
Willian <willian.alves.b15@gmail.com>
2022-10-11 19:55:22 -0300

    feat: req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    students = 0
+    try:
+        for hour in permanence_period:
+            if hour[0] <= target_time <= hour[1]:
+                students += 1
+    except TypeError:
+        return None
+    return students
