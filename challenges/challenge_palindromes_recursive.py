d1cd19861231742a59ab487bf94603f88ee9f63a
Willian <willian.alves.b15@gmail.com>
2022-10-14 10:36:00 -0300

    feat: req 02

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    int_low_index = int(low_index)
+    int_high_index = int(high_index)
+    if len(word) == 0:
+        return False
+    elif word[int_low_index] != word[int_high_index]:
+        return False
+    elif int_low_index >= int_high_index:
+        return True
+    else:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
