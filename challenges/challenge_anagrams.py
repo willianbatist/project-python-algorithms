2a9bc900a5f759b4fe5576322b258e2ef578d76d
Willian <willian.alves.b15@gmail.com>
2022-10-17 16:28:46 -0300

    feat: req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    if first_string == '' or second_string == '':
+        return False
+
+    first = list(first_string.lower())
+    second = list(second_string.lower())
+
+    if len(first) != len(second_string):
+        return False
+
+    for row in first:
+        if row in second:
+            second.remove(row)
+        else:
+            return False
+    return True
