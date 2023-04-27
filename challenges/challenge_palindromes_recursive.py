8684892208f11f6e0d1751165104029f18020bba
Willian Batista <willian.alves.b15@gmail.com>
2023-04-27 20:18:41 -0300

    Update challenge_palindromes_recursive.py

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -8,4 +8,4 @@ def is_palindrome_recursive(word, low_index, high_index):
     elif int_low_index >= int_high_index:
         return True
     else:
-        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
