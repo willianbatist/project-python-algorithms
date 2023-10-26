fbb764bf2d6c10351b4d0d89e1808826a6176fd5
Willian Batista <willian.alves.b15@gmail.com>
2023-10-26 09:33:59 -0300

    Update challenge_study_schedule.py

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,5 +5,4 @@ def study_schedule(permanence_period, target_time):
             if hour[0] <= target_time <= hour[1]:
                 students += 1
     except TypeError:
-        return None
-    return students
+
